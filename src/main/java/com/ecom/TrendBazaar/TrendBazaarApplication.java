c9786a85df43f717706634d94b331edad02c9936
Salman siddiqui <salmansiddiqui80900@gmail.com>
2024-08-12 20:21:07 +0530

    This is new e-commerce project

---
+++ src/main/java/com/ecom/TrendBazaar/TrendBazaarApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.TrendBazaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrendBazaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrendBazaarApplication.class, args);
+	}
+
+}
