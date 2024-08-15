8846a12fd132bf633303bf1b0f444ccc66649d97
Salman siddiqui <salmansiddiqui80900@gmail.com>
2024-08-15 11:38:09 +0530

    Add some frontend code

--- src/main/java/com/ecom/TrendBazaar/controllers/AdminController.java
+++ src/main/java/com/ecom/TrendBazaar/controllers/AdminController.java
@@ -4,10 +4,19 @@ import com.ecom.TrendBazaar.model.Category;
 import com.ecom.TrendBazaar.service.CategoryService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 @RequestMapping("/admin")
@@ -28,16 +37,16 @@ public class AdminController
     }
 
     @GetMapping("/getAddCategory")
-    public String getAddCategory()
+    public String getAddCategory(Model model)
     {
+        model.addAttribute("categories",categoryService.getAllCategory());
         return "admin/add_category";
     }
 
     @PostMapping("/saveCategory")
-    public String saveCategory(@ModelAttribute Category category , @RequestParam("image") MultipartFile file, HttpSession httpSession)
-    {
-        String imageName = file != null ? file.getOriginalFilename():"default.jpg";
-        category.setImage(imageName);
+    public String saveCategory(@ModelAttribute Category category , @RequestParam("file") MultipartFile file, HttpSession httpSession) throws IOException {
+        String image = file != null ? file.getOriginalFilename() : "default.jpg";
+        category.setImage(image);
         if(categoryService.exitCategory(category.getName()))
         {
             httpSession.setAttribute("errorMsg","Category already exits");
@@ -49,9 +58,13 @@ public class AdminController
                 httpSession.setAttribute("errorMsg","Not saved ! Facing some issue");
             }
             else{
+                File saveFile = new ClassPathResource("static/img/img").getFile();
+                Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "category_img" + File.separator + file.getOriginalFilename());
+                System.out.println(path);
+                Files.copy(file.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
                 httpSession.setAttribute("successMsg","Category save successfully ");
             }
         }
-        return "redirect:/getAddCategory";
+        return "redirect:/admin/getAddCategory";
     }
 }
---
+++ src/main/java/com/ecom/TrendBazaar/service/CommonService.java
@@ -0,0 +1,9 @@
+package com.ecom.TrendBazaar.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface CommonService
+{
+    public void removeSessionMessage();
+}
